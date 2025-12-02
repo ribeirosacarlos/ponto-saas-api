aa801ebd11b71b9aac1e555f5dad934c976392cf
Carlos de Sá <ribeirosacarlos@gmail.com>
2025-12-01 21:10:00 -0300

    fix: company

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as FakerFactory;
 
 class CompanyFactory extends Factory
 {
@@ -11,9 +12,11 @@ class CompanyFactory extends Factory
 
     public function definition(): array
     {
+        $faker = FakerFactory::create(); // instancia o Faker "na mão"
+
         return [
-            'name' => fake()->company(),
-            'slug' => fake()->unique()->slug(),
+            'name' => $faker->company(),
+            'slug' => $faker->unique()->slug(),
         ];
     }
 }
