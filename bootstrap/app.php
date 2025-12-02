63165db83253ff01d6e23ed73736168e8866fa31
Carlos de Sá <ribeirosacarlos@gmail.com>
2025-12-01 21:54:15 -0300

    fix: cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->prepend(HandleCors::class);
     })   
     ->withExceptions(function (Exceptions $exceptions): void {
         //
