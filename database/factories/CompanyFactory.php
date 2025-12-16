a272f1f8bd6cbd2a8f42d260a9893c51efcb0aed
Carlos de Sá <ribeirosacarlos@gmail.com>
2025-12-16 00:09:47 -0300

    fix: ultimo commit

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -13,19 +13,20 @@ class CompanyFactory extends Factory
     public function definition(): array
     {
         $name = 'Empresa ' . Str::random(5);
+        $faker = fake();
 
         return [
             'name' => $name,
             'slug' => Str::slug($name),
-            'document' => $this->faker->numerify('##############'),
-            'email' => $this->faker->unique()->safeEmail(),
-            'phone' => $this->faker->phoneNumber(),
-            'address' => $this->faker->streetAddress(),
-            'city' => $this->faker->city(),
-            'state' => $this->faker->stateAbbr(),
-            'plan' => $this->faker->randomElement(['free', 'pro', 'enterprise']),
-            'trial_ends_at' => $this->faker->optional()->dateTimeBetween('-7 days', '+30 days'),
-            'subscription_ends_at' => $this->faker->optional()->dateTimeBetween('now', '+60 days'),
+            'document' => $faker->numerify('##############'),
+            'email' => $faker->unique()->safeEmail(),
+            'phone' => $faker->phoneNumber(),
+            'address' => $faker->streetAddress(),
+            'city' => $faker->city(),
+            'state' => $faker->stateAbbr(),
+            'plan' => $faker->randomElement(['free', 'pro', 'enterprise']),
+            'trial_ends_at' => $faker->optional()->dateTimeBetween('-7 days', '+30 days'),
+            'subscription_ends_at' => $faker->optional()->dateTimeBetween('now', '+60 days'),
             'is_blocked' => false,
             'blocked_at' => null,
             'blocked_reason' => null,
