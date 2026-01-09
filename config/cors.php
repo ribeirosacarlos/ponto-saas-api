5833820075df205603b82bb9f239f33754970e70
Carlos de Sá <ribeirosacarlos@gmail.com>
2026-01-09 19:35:04 -0300

    feat: liberando cors

--- config/cors.php
+++ config/cors.php
@@ -9,8 +9,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://api.jornafy.com',
-        'https://app.jornafy.com',
+        'https://jornafy.com',
     ],
 
     'allowed_origins_patterns' => [],
