1d48d876ad338b4fd78c9bce2610b8619b49db07
Carlos de Sá <ribeirosacarlos@gmail.com>
2026-01-10 12:31:49 -0300

    feat: ajuste cors

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://api.jornafy.com',
+        'https://app.jornafy.com',
         'https://jornafy.com',
     ],
 
