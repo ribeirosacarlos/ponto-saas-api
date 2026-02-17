abb2fd5ee2d925dd795cce58e72fc5643b7155f7
ribeirosacarlos <ribeirosacarlos@gmail.com>
2026-02-17 19:52:50 -0300

    fix: filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -77,4 +77,15 @@ return [
         public_path('storage') => storage_path('app/public'),
     ],
 
+
+    's3' => [
+        'driver' => 's3',
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION'),
+        'bucket' => env('AWS_BUCKET'),
+        'throw' => true,
+    ],
+
+
 ];
