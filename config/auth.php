baca1c13ae6e904a2fde51d32fce16271cdd27e6
ribeirosacarlos <83379587+ribeirosacarlos@users.noreply.github.com>
2026-06-30 17:18:01 -0300

    fix: Add affiliates configuration to auth.php

--- config/auth.php
+++ config/auth.php
@@ -107,6 +107,12 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],
+        'affiliates' => [
+            'provider' => 'affiliates',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
     ],
 
     /*
