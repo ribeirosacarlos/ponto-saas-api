f6ba5f952ba33d32c1d24405e5dbc2653436ec90
Carlos de Sá <ribeirosacarlos@gmail.com>
2026-08-14 15:47:30 -0300

    chore: add Hostinger homologation config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,6 +19,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(
+            at: env('TRUSTED_PROXIES'),
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+        );
         $middleware->appendToGroup('api', [
             \App\Http\Middleware\ForceJsonResponse::class,
             \App\Http\Middleware\IdentifyTenant::class,
